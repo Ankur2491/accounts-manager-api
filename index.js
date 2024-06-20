7600c8281cf85f7d4d7d1f66128c922b34eaa6cf
Ankur Sharma <ankur.101045@gmail.com>
2024-06-20 21:26:43 +0530

    modified the method

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ app.post("/retrieveHitWords", async(req, res) => {
     for(let desc of allDescription) {
         let tokens = tokenizer.tokenize(desc);
         for (let token of tokens) {
-            if (token.length() > 2) {
+            if (token.length > 2) {
                 if (!stopWords.some(word => word.toLowerCase() === token.toLowerCase())) {
                     if (freqObject[token]) {
                         freqObject[token] += 1
